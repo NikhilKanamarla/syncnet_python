d67c43d198a23ff5909bb336332ae703c6e4e3fa
nkanama <nkanama@umich.edu>
2020-12-16 16:03:14 -0500

    updated trail run to fill up real data

--- trialrun.py
+++ trialrun.py
@@ -38,13 +38,13 @@ def checkValid(name):
     json_check_3 = open(json_file_3)
     json_check_3X = json.load(json_check_3)
     for (key, values) in json_check_1X.items():
-        if(key == name and values['label'] == 'REAL'):
+        if(key == name and values['label'] == 'FAKE'):
             realOrFake = True
     for (key, values) in json_check_2X.items():
-        if(key == name and values['label'] == 'REAL'):
+        if(key == name and values['label'] == 'FAKE'):
             realOrFake = True
     for (key, values) in json_check_3X.items():
-        if(key == name and values['label'] == 'REAL'):
+        if(key == name and values['label'] == 'FAKE'):
             realOrFake = True
     json_check_1.close()
     json_check_2.close()
@@ -56,9 +56,9 @@ def data_test():
     # Iterate through fake data directory and run through model
     directory = '/datab/nkanama/facebookDataset/trialRun/data'
     # output directory for fake data
-    output_directory = '/datac/nkanama/facebookDataset/output_model_fake'
+    #output_directory = '/datac/nkanama/facebookDataset/output_model_fake'
     # output directory for real data
-    #output_directory = '/datac/nkanama/facebookDataset/output_model__real'
+    output_directory = '/datac/nkanama/facebookDataset/output_model__real'
     videofile = ' '
     reference = ' '
     num = 0
@@ -75,7 +75,7 @@ def data_test():
         else:
             num_videos = num_videos+1
             num = 0
-        if(num_videos == 19):
+        if(num_videos == 17):
             break
 
         videofile = os.path.join(directory, video)
